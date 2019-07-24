2ed8f4f49fba5ce83dc96b690cdc74093626bc3c
Bruno Bhering <bruno.bhering@ageri.com.br>
2019-07-24 16:33:14 -0300

    first commit

---
+++ zabbix_api.py
@@ -0,0 +1,28 @@
+import requests, json, os, sys
+from pyzabbix.api import ZabbixAPI
+
+template = sys.argv
+dicionario = {}
+
+def get(user, password, template):
+    url = 'https://noc.ageri.com.br/'
+    zabbix = ZabbixAPI(url=url, user=user, password=password)
+    f = zabbix.do_request('template.get', {'filter': {'host': template},'output': 'templateid'})
+    templateid = f['result'][0]['templateid']
+    item = zabbix.do_request('item.get',{'templateids': templateid,'output': 'extend'})
+    for n in item['result']:
+         dicionario.update({ n['name']: n['key_']})
+    zabbix.user.logout()
+
+def get_items(template):
+    template = template
+    user = os.environ['USER']
+    password = os.environ['PASSWORD']
+    get(user, password, template)
+
+def main():
+    get_items(template)
+    print(json.dumps(dicionario, indent=4, sort_keys=True))
+
+if __name__ == "__main__":
+    main()
